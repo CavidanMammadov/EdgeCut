f1e031b3a6390775aa1a585abd2abb90ae193243
CavidanMammadov <cavidanbm-bp215@code.edu.az>
2024-11-26 03:26:49 +0400

    Edgecut Front to back

---
+++ EdgeCutEmpty/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EdgeCutEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Furniture()
+        {
+            return View();
+        }
+        public IActionResult Blogs()
+        {
+            return View();
+        }
+    }
+}
